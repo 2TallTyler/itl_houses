7b2576fd8bdb86f6fe6c1e81840c52faf8591f58
Tyler Trahan <tyler@tylertrahan.com>
2020-11-18 19:26:41 -0500

    Update MAKEFILE for new build machine

--- MAKEFILE.bat
+++ MAKEFILE.bat
@@ -26,6 +26,6 @@ if /I "%c%" EQU "TT" (goto :copy
 
 :copy
 ECHO Copying to NewGRF directory
-copy /y /v %~dp0\improved_town_layouts.grf C:\Users\train\Documents\OpenTTD\newgrf\
+copy /y /v %~dp0\improved_town_layouts.grf C:\Users\tyler\Documents\OpenTTD\newgrf\
 pause
 exit
